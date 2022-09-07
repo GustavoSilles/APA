f0bb44987d7afb797b1d8610e4150855af41efdd
GustavoSilles <92472220+GustavoSilles@users.noreply.github.com>
2022-09-07 00:46:47 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,63 @@
+// import React, {StrictMode} from 'react';
+// import {createRoot} from 'react-dom/client';
+
+// import App from './App';
+
+// // 👇️ IMPORTANT: use correct ID of your root element
+// // this is the ID of the div in your index.html file
+// const rootElement = document.getElementById('root');
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// import Feed from './routes/FeedPage'
+// import Home3Page from './routes/Home3Page'
+// import PerfilPage from './routes/PerfilPage'
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path='/' element={<App />} />
+//       <Route path='/feed' element={<Feed />} />
+//       <Route path='/home3' element={<Home3Page />} />
+//       <Route path='/perfil' element={<PerfilPage />} />
+//     </Routes>
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
+
