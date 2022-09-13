b96082ff78a9928437cfb48fb90ad2c420788442
GustavoSilles <sillesgustavo05@gmail.com>
2022-09-13 15:34:30 -0300

    esqueci oque mudei

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -20,7 +20,6 @@ const Navbar = () => {
                     <li><Link to='/feed'>Feed</Link> </li>
                     <li><Link to='/apimapa'>bglh do maps</Link></li>
                     <li><Link to='/perfil'>Perfil</Link></li>
-                    <Link to='/login'><button className='navbtn'>Logout</button></Link>
                 </ul>
                 <div className="hamburger" onClick={handleNav}>
                     {!nav ? (<FaBars className='icon' />) : (<FaTimes className='icon' />)}
