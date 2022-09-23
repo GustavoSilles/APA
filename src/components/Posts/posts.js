601d672c54bf4111ac44a4168939cd31a831ee5e
GustavoSilles <sillesgustavo05@gmail.com>
2022-09-22 23:42:46 -0300

    feed lindo e nada responsivo arrumei a responsa da landinpage

---
+++ src/components/Posts/posts.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './posts.css'
+import {BsFillChatLeftTextFill} from "react-icons/bs"
+import {BsFillShareFill} from "react-icons/bs"
+
+const Posts = () => {
+    return (
+       
+       <>
+       <div className='postzao'>
+                <div className='posixion'>
+                <div className='box'>
+                    <div className='navbox'>
+                        <div className='fotopost2'></div>
+                        <div className='nominho'>Gustavin peroca mansa</div>
+                    </div>
+                    <div className='descricao'>
+                        <p className='descrison'>oia que gatinho bunitu</p>
+                    </div>
+                    <div className='imgbox'>
+                        <div className='propriafoto'></div>
+                    </div>
+                    <div className='footerbox'>
+                    <div className='ascoisas'>
+                    <BsFillChatLeftTextFill className='iconbox' />
+                    <div className='textbox'>Comente</div>
+                    </div>
+                    <div className='ascoisas2'>
+                    <BsFillShareFill className='iconbox'/>
+                    <div className='textbox'>Compartilhe</div>
+                    </div>
+                    </div>
+                    <div className='comentario'></div>
+                </div>
+                </div>
+
+                </div>
+                </> 
+                
+      
+    )
+}
+
+export default Posts
