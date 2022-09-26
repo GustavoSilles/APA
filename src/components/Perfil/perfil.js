dfb6a92aad0a4b5d27f247ea4768a5fdb134115c
GustavoSilles <sillesgustavo05@gmail.com>
2022-09-26 17:31:19 -0300

    so colocando a navbar no perfil

--- src/components/Perfil/perfil.js
+++ src/components/Perfil/perfil.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import './perfilStyles.css'
-
+import Navbar from '../Navbar/navbar'
+import Footer from '../Footer/footer'
 const Perfil = () => {
     return (
+        <>
+        <Navbar/>
         <div className='perfil'>
             <div className="container">
-                <div className="form-container">
-                    <form>
-                       
-                    </form>
+              
                 </div>
             </div>
-        </div>
+            <Footer/>
+        </>
     )
 }
 
