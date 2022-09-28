a97df1d10778fe8ba5f0c73a473707bf020a2af0
GustavoSilles <sillesgustavo05@gmail.com>
2022-09-28 15:40:57 -0300

    alterando coisas

--- src/components/Posts/posts.js
+++ src/components/Posts/posts.js
@@ -12,7 +12,7 @@ const Posts = () => {
                 <div className='box'>
                     <div className='navbox'>
                         <div className='fotopost2'></div>
-                        <div className='nominho'>Gustavin peroca mansa</div>
+                        <div className='nominho'>Gustavin</div>
                     </div>
                     <div className='descricao'>
                         <p className='descrison'>oia que gatinho bunitu Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
