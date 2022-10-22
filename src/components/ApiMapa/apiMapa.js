081ca350a083535749acf2be8f727a76b66fad10
AbilioGamaNJ <abiliogamanetojunior@gmail.com>
2022-10-22 17:29:33 -0300

    Mudança na api, falta marcadores

--- src/components/ApiMapa/apiMapa.js
+++ src/components/ApiMapa/apiMapa.js
@@ -3,16 +3,19 @@ import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import './apiMapaStyles.css'
 import Navbar from '../Navbar/navbar'
 
-
 const containerStyle = {
     width: '100%',
     height: '100vh'
   };
   
   const center = {
-    lat:  -27.5969,
-    lng: -48.5495 
+    lat:  -27.590824,
+    lng: -48.551262 
   };
+  // const petshop = {
+  //   lat: -27.431461,
+  //   lng: -48.396598
+  // };
   
 const ApiMapa = () => {
     
@@ -20,15 +23,21 @@ const ApiMapa = () => {
         <>
     <Navbar/>
      <LoadScript
-      // googleMapsApiKey="AIzaSyDrTIZQcdJFV8sv5VwV-S4v2YOgtO3AcfU"
-      googleMapsApiKey="AIzaSyBR9lQa8zmcvvzbh1mbbetR7t59-pjCSxk"
+      googleMapsApiKey="AIzaSyDrTIZQcdJFV8sv5VwV-S4v2YOgtO3AcfU"
+      // googleMapsApiKey="AIzaSyBR9lQa8zmcvvzbh1mbbetR7t59-pjCSxk"
     >
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={12}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
+        {/* <Marker position={petshop} />
+        <Marker position={center} options={{
+          label: {
+            text: "Posição teste",
+            className: "map-marker"
+          }
+        }} /> */}
         <></>
       </GoogleMap>
        </LoadScript> 
