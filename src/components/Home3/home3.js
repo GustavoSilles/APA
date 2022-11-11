58661e42fadf58aa213c42d38a463e5c52cc0103
ABÍLIO GAMA NETO JÚNIOR <abilio_neto-junior@estudante.sesisenai.org.br>
2022-11-11 16:47:40 -0300

    Mudanças no Leia Mais, entre outros

--- src/components/Home3/home3.js
+++ src/components/Home3/home3.js
@@ -9,7 +9,7 @@ const Home3 = () => {
                     <h2><span className='grayText'>Ame</span> os animais</h2>
                     <p>Mesmo quando estamos nos sentindo sozinhos e sem nenhum amigo, nossos animais sempre estarão
                        conosco. Nós precisamos mais deles do que ele de nós, e para que um possa cuidar do outro, 
-                       é preciso que você adote e cuide bem de seu animalzinho. Caso possua dúvidas, 
+                       é preciso que você adote e cuide bem de seu animal. Caso possua dúvidas, 
                        basta entrar em contato com nossa equipe. 
                     </p>
                 <div><button className='grayButton'>Contatar</button></div>
