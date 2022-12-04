b120f9e0c70e517fdb637a4e03ad000092f2011b
AbilioGamaNJ <abiliogamanetojunior@gmail.com>
2022-12-03 22:33:05 -0300

    Mudanças em nada

--- src/components/Adm/adm.js
+++ src/components/Adm/adm.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import './adm.css'
 import Navbar2 from '../Navbar2/navbar2'
-import Posts2 from '../Posts2/posts2'
+
 const Adm = () => {
    
 
